f9f00658aa4775ca2883c762dabefca2b99e8e80
anddani <andredanielsson93@gmail.com>
2018-06-29 14:21:34 +0200

    Support both batteries in status bar

--- .xmonad/scripts/bar_battery.sh
+++ .xmonad/scripts/bar_battery.sh
@@ -2,30 +2,43 @@
 
 . $(dirname $0)/config.sh
 
+function battery_color {
+    BAT_PERC="$1"
+    STATUS="$2"
+    TIME="$3"
+    if [ "$BAT_PERC" -lt "10" ]; then
+	COLOR="^fg($warning)"
+    elif [ "$BAT_PERC" -lt "40" ]; then
+	COLOR="^fg(#FFFC7F)"
+    else
+	COLOR="^fg(#32CD32)"
+    fi
+
+    ret="$COLOR$BAT_PERC%^fg()"
+
+    # If discharging, show time left
+    if [ "$STATUS" = "Discharging" ]; then
+	TIME_LEFT=$(echo "$TIME" | sed 's/\([0-9]*:[0-9]*\).*/\1/')
+	ret="$ret ($TIME_LEFT)"
+    fi
+    echo "$ret"
+}
+
 BAT=$(acpi -b | awk '{gsub(/%,/,""); print $4}' | sed 's/%//g')
 STATUS=$(acpi -b | awk '{gsub(/,/,""); print $3}')
-color=""
 
-# TODO: Analyze which battery is active and show a (x/y)
-BAT1=$(echo $BAT | awk '{print $1}')
-# BAT2=$(echo $BAT | awk '{print $2}')
+# Time left
+TIME0=$(acpi -b | awk '{gsub(/%,/,""); print $5}' | head -n1)
+TIME1=$(acpi -b | awk '{gsub(/%,/,""); print $5}' | tail -n1)
 
-if [ "$BAT1" -lt "10"  ]; then
-    ICON="bat_empty_01.xbm"
-    color="^fg($warning)"
-    BATBAR=$(echo -e "$BAT1" | gdbar -bg $bar_bg -fg $warning -h 4 -w 50)
-else
-    if [ "$BAT1" -lt "40" ]; then
-	ICON="bat_low_01.xbm"
-    else
-	ICON="bat_full_01.xbm"
-    fi
-    BATBAR=$(echo -e "$BAT1" | gdbar -bg $bar_bg -fg $bar_fg -h 4 -w 50)
-fi
+BAT0_PERC=$(echo $BAT | awk '{print $1}')
+BAT1_PERC=$(echo $BAT | awk '{print $2}')
 
-if [[ $STATUS != "Discharging" ]]; then
-    ICON="ac.xbm"
-fi
+# Discharging | Full
+STATUS0=$(echo $STATUS | awk '{print $1}')
+STATUS1=$(echo $STATUS | awk '{print $2}')
 
-ICON="^i($HOME/.xmonad/icons/$ICON)"
-echo "$color$ICON $BATBAR"
+BAT0=$(battery_color "$BAT0_PERC" "$STATUS0" "$TIME0")
+BAT1=$(battery_color "$BAT1_PERC" "$STATUS1" "$TIME1")
+
+echo "Bat0: $BAT0 Bat1: $BAT1"
